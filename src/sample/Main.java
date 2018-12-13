e0fe74616e0d19b130133b84cc4228291914abde
clam3154 <clam3154@bths.edu>
2018-12-13 10:11:15 -0500

    /...

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,7 +13,6 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        Button button = new Button("Click Me");
         primaryStage.show();
     }
 
